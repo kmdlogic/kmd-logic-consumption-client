bd6606ba1bcd7deb32ade98c81aa365bb86dbd2c
Basavaraj Araballi (XQK) <XQK@kmd.dk>
2019-06-03 08:53:46 +0530

    Reserved Capcity implemented

---
+++ src/Kmd.Logic.Consumption.Client.AuditClient/AuditClientReservedCapcityMetricsDestination.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Kmd.Logic.Audit.Client;
+
+namespace Kmd.Logic.Consumption.Client.AuditClient
+{
+    public class AuditClientReservedCapcityMetricsDestination : IReservedCapcityMetricsDestination
+    {
+        public static string Template { get; } = "Consumed {Amount} for {Meter} on resource {ResourceId} in subscription {SubscriptionId}";
+
+        public static string GetDefaultSubOwnerContextName(string propertyName) => $"__Sub_{propertyName}";
+
+        private readonly IAudit _audit;
+        private readonly Func<string, string> _getSubOwnerContextName;
+
+        public AuditClientReservedCapcityMetricsDestination(IAudit audit, Func<string, string> getSubOwnerContextName = null)
+        {
+            this._audit = audit ?? throw new ArgumentNullException(nameof(audit));
+            this._getSubOwnerContextName = getSubOwnerContextName ?? GetDefaultSubOwnerContextName;
+        }
+
+        public IReservedCapcityMetricsDestination ForInternalContext(string propertyName, string value)
+        {
+            return new AuditClientReservedCapcityMetricsDestination(
+               this._audit.ForContext(propertyName, value),
+               this._getSubOwnerContextName);
+        }
+
+        public IReservedCapcityMetricsDestination ForSubscriptionOwnerContext(string propertyName, string value)
+        {
+            return new AuditClientReservedCapcityMetricsDestination(
+                 this._audit.ForContext(this._getSubOwnerContextName(propertyName), value),
+                 this._getSubOwnerContextName);
+        }
+
+        public void ReleaseCapacity(Guid subscriptionId, Guid resourceId, string meter, int amount, string reason = null)
+        {
+            var audit = reason == null
+                       ? this._audit
+                       : this._audit.ForContext("Reason", reason);
+
+            audit = audit.ForContext("Capacity", "Release");
+
+            audit.Write(
+                Template,
+                amount,
+                meter,
+                resourceId,
+                subscriptionId);
+        }
+
+        public void ReserveCapacity(Guid subscriptionId, Guid resourceId, string meter, int amount, string reason = null)
+        {
+            var audit = reason == null
+                       ? this._audit
+                       : this._audit.ForContext("Reason", reason);
+
+            audit = audit.ForContext("Capacity", "Reserve");
+
+            audit.Write(
+                Template,
+                amount,
+                meter,
+                resourceId,
+                subscriptionId);
+        }
+    }
+}
---
+++ src/Kmd.Logic.Consumption.Client/ReservedCapacityMetrics.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kmd.Logic.Consumption.Client
+{
+    public class ReservedCapacityMetrics : IReservedCapacityMetrics
+    {
+        private readonly IReservedCapcityMetricsDestination _destination;
+
+        public ReservedCapacityMetrics(IReservedCapcityMetricsDestination destination)
+        {
+            this._destination = destination ?? throw new ArgumentNullException(nameof(destination));
+        }
+
+        public void Decrease(Guid subscriptionId, Guid resourceId, DateTimeOffset dateTime, string meter, int amount, string reason = null)
+        {
+            this._destination.ForSubscriptionOwnerContext(
+               propertyName: "DecreaseDateTime",
+               value: dateTime.UtcTicks.ToString(System.Globalization.CultureInfo.InvariantCulture));
+
+            this._destination.ReserveCapacity(
+                subscriptionId: subscriptionId,
+                resourceId: resourceId,
+                meter: meter,
+                amount: amount,
+                reason: reason);
+        }
+
+        public IReservedCapacityMetrics ForInternalContext(string name, string value)
+        {
+            return new ReservedCapacityMetrics(this._destination
+               .ForInternalContext(
+                   propertyName: name,
+                   value: value));
+        }
+
+        public IReservedCapacityMetrics ForSubscriptionOwnerContext(string name, string value)
+        {
+            return new ReservedCapacityMetrics(this._destination
+               .ForSubscriptionOwnerContext(
+                   propertyName: name,
+                   value: value));
+        }
+
+        public void Increase(Guid subscriptionId, Guid resourceId, DateTimeOffset dateTime, string meter, int amount, string reason = null)
+        {
+            this._destination.ForSubscriptionOwnerContext(
+                propertyName: "IncreaseDateTime",
+                value: dateTime.UtcTicks.ToString(System.Globalization.CultureInfo.InvariantCulture));
+
+            this._destination.ReserveCapacity(
+                subscriptionId: subscriptionId,
+                resourceId: resourceId,
+                meter: meter,
+                amount: amount,
+                reason: reason);
+        }
+    }
+}
