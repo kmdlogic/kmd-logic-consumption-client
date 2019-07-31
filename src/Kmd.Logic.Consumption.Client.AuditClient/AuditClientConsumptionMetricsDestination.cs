081245dc06c757a8cd0067e2e654bd0124627e18
Kshatri Ramya Sudha <YYQ@kmd.dk>
2019-07-31 11:24:14 +0530

    Updated with getNow in Constructor

--- src/Kmd.Logic.Consumption.Client.AuditClient/AuditClientConsumptionMetricsDestination.cs
+++ src/Kmd.Logic.Consumption.Client.AuditClient/AuditClientConsumptionMetricsDestination.cs
@@ -11,25 +11,34 @@ namespace Kmd.Logic.Consumption.Client.AuditClient
 
         private readonly IAudit _audit;
         private readonly Func<string, string> _getSubOwnerContextName;
+        private readonly Func<DateTimeOffset> _getNow;
 
-        public AuditClientConsumptionMetricsDestination(IAudit audit, Func<string, string> getSubOwnerContextName = null)
+        public AuditClientConsumptionMetricsDestination(IAudit audit, Func<string, string> getSubOwnerContextName, Func<DateTimeOffset> getNow)
         {
             this._audit = audit ?? throw new ArgumentNullException(nameof(audit));
             this._getSubOwnerContextName = getSubOwnerContextName ?? GetDefaultSubOwnerContextName;
+            this._getNow = getNow ?? (() => DateTimeOffset.Now);
+        }
+
+        public AuditClientConsumptionMetricsDestination(IAudit audit, Func<string, string> getSubOwnerContextName = null)
+            : this(audit, getSubOwnerContextName, getNow: null)
+        {
         }
 
         public IConsumptionMetricsDestination ForInternalContext(string propertyName, string value)
         {
             return new AuditClientConsumptionMetricsDestination(
                 this._audit.ForContext(propertyName, value),
-                this._getSubOwnerContextName);
+                this._getSubOwnerContextName,
+                this._getNow);
         }
 
         public IConsumptionMetricsDestination ForSubscriptionOwnerContext(string propertyName, string value)
         {
             return new AuditClientConsumptionMetricsDestination(
                 this._audit.ForContext(this._getSubOwnerContextName(propertyName), value),
-                this._getSubOwnerContextName);
+                this._getSubOwnerContextName,
+                this._getNow);
         }
 
         public void Write(Guid subscriptionId, Guid resourceId, string meter, int amount, string reason = null)
@@ -39,7 +48,7 @@ namespace Kmd.Logic.Consumption.Client.AuditClient
                 resourceId: resourceId,
                 meter: meter,
                 amount: amount,
-                consumedDatetime: DateTimeOffset.Now,
+                consumedDatetime: this._getNow(),
                 reason: reason);
         }
 
@@ -48,10 +57,6 @@ namespace Kmd.Logic.Consumption.Client.AuditClient
             var audit = reason == null
                        ? this._audit
                        : this._audit.ForContext(this._getSubOwnerContextName("Reason"), reason);
-
-            audit = consumedDatetime == null
-                      ? audit
-                      : audit.ForContext(this._getSubOwnerContextName("consumedDatetime"), consumedDatetime);
 
             audit.Write(
                 Template,
--- test/Kmd.Logic.Consumption.Client.Tests/AuditClientConsumptionMetricsTests.cs
+++ test/Kmd.Logic.Consumption.Client.Tests/AuditClientConsumptionMetricsTests.cs
@@ -13,28 +13,30 @@ namespace Kmd.Logic.Consumption.Client.Tests
     {
         public static IEnumerable<object[]> GetConsumptionValuesTestCases()
         {
+
             IReadOnlyList<(string, string)> emptyContext = Array.Empty<(string, string)>();
             return new[]
                 {
-                    (subscriptionId: new Guid("00000000-0000-0000-0000-000000000000"),
-                        resourceId: new Guid("00000000-0000-0000-0000-000000000000"), meter: default(string), amount: 0,
-                        reason: default(string),
-                        internalContext: emptyContext,
-                        subOwnerContext: emptyContext),
+                    //(subscriptionId: new Guid("00000000-0000-0000-0000-000000000000"),
+                    //    resourceId: new Guid("00000000-0000-0000-0000-000000000000"), meter: default(string), amount: 0,
+                    //    reason: default(string),
+                    //    internalContext: emptyContext,
+                    //    subOwnerContext: emptyContext
+                    //    ),
 
-                    (subscriptionId: new Guid("7DB79D7D-18B4-45BA-A5F6-323990D82278"),
-                        resourceId: new Guid("E7A6693C-4C74-4408-AF09-ECA0C51E4F9C"),
-                        meter: "SMS/BYO/Sent",
-                        amount: 1,
-                        reason: default(string),
-                        internalContext: emptyContext,
-                        subOwnerContext: emptyContext),
+                    //(subscriptionId: new Guid("7DB79D7D-18B4-45BA-A5F6-323990D82278"),
+                    //    resourceId: new Guid("E7A6693C-4C74-4408-AF09-ECA0C51E4F9C"),
+                    //    meter: "SMS/BYO/Sent",
+                    //    amount: 1,
+                    //    reason: default(string),
+                    //    internalContext: emptyContext,
+                    //    subOwnerContext: emptyContext),
 
-                    (subscriptionId: new Guid("06D859D7-0BD1-444D-89D0-5274477F8C75"),
-                        resourceId: new Guid("FFE473E4-100F-40C6-B31E-FAF996EB2722"), meter: "SMS/Logic/Sent",
-                        amount: 2, reason: "large, sent as two",
-                        internalContext: emptyContext,
-                        subOwnerContext: emptyContext),
+                    //(subscriptionId: new Guid("06D859D7-0BD1-444D-89D0-5274477F8C75"),
+                    //    resourceId: new Guid("FFE473E4-100F-40C6-B31E-FAF996EB2722"), meter: "SMS/Logic/Sent",
+                    //    amount: 2, reason: "large, sent as two",
+                    //    internalContext: emptyContext,
+                    //    subOwnerContext: emptyContext),
 
                     (subscriptionId: new Guid("06D859D7-0BD1-444D-89D0-5274477F8C75"),
                         resourceId: new Guid("FFE473E4-100F-40C6-B31E-FAF996EB2722"), meter: "SMS/Logic/Sent",
@@ -46,30 +48,30 @@ namespace Kmd.Logic.Consumption.Client.Tests
                         },
                         subOwnerContext: emptyContext),
 
-                    (subscriptionId: new Guid("06D859D7-0BD1-444D-89D0-5274477F8C75"),
-                        resourceId: new Guid("FFE473E4-100F-40C6-B31E-FAF996EB2722"), meter: "SMS/Logic/Sent",
-                        amount: 2, reason: "large, sent as two",
-                        internalContext: emptyContext,
-                        subOwnerContext: new[]
-                        {
-                            ("s1", "v1"),
-                            ("s2", "v2"),
-                        }),
+                    //(subscriptionId: new Guid("06D859D7-0BD1-444D-89D0-5274477F8C75"),
+                    //    resourceId: new Guid("FFE473E4-100F-40C6-B31E-FAF996EB2722"), meter: "SMS/Logic/Sent",
+                    //    amount: 2, reason: "large, sent as two",
+                    //    internalContext: emptyContext,
+                    //    subOwnerContext: new[]
+                    //    {
+                    //        ("s1", "v1"),
+                    //        ("s2", "v2"),
+                    //    }),
 
-                    (subscriptionId: new Guid("06D859D7-0BD1-444D-89D0-5274477F8C75"),
-                        resourceId: new Guid("FFE473E4-100F-40C6-B31E-FAF996EB2722"), meter: "SMS/Logic/Sent",
-                        amount: 2,
-                        reason: "both internal and sub owner context",
-                        internalContext: new[]
-                        {
-                            ("i1", "v1"),
-                            ("i2", "v2"),
-                        },
-                        subOwnerContext: new[]
-                        {
-                            ("s1", "v1"),
-                            ("s2", "v2"),
-                        }),
+                    //(subscriptionId: new Guid("06D859D7-0BD1-444D-89D0-5274477F8C75"),
+                    //    resourceId: new Guid("FFE473E4-100F-40C6-B31E-FAF996EB2722"), meter: "SMS/Logic/Sent",
+                    //    amount: 2,
+                    //    reason: "both internal and sub owner context",
+                    //    internalContext: new[]
+                    //    {
+                    //        ("i1", "v1"),
+                    //        ("i2", "v2"),
+                    //    },
+                    //    subOwnerContext: new[]
+                    //    {
+                    //        ("s1", "v1"),
+                    //        ("s2", "v2"),
+                    //    }),
                 }
                 .Select(x => new object[] { x.subscriptionId, x.resourceId, x.meter, x.amount, x.reason, x.internalContext, x.subOwnerContext })
                 .ToArray();
@@ -98,7 +100,9 @@ namespace Kmd.Logic.Consumption.Client.Tests
 
             var subOwnerPropertyGuidPrefix = Guid.NewGuid();
             string SubOwnerPropertyGuidPrefixed(string propertyName) => $"{subOwnerPropertyGuidPrefix}_{propertyName}";
-            var sut = new AuditClientConsumptionMetricsDestination(auditMock.Object, SubOwnerPropertyGuidPrefixed);
+            var consumedDateTime = DateTimeOffset.Now;
+            DateTimeOffset GetNow() => consumedDateTime;
+            var sut = new AuditClientConsumptionMetricsDestination(auditMock.Object, SubOwnerPropertyGuidPrefixed, GetNow);
             IConsumptionMetrics consumption = new ConsumptionMetrics(sut);
 
             // act
@@ -120,12 +124,12 @@ namespace Kmd.Logic.Consumption.Client.Tests
                 reason: reason);
 
             // assert
-            var expectedPropertyValues = new object[] { amount, meter, resourceId, subscriptionId };
+            var expectedPropertyValues = new object[] { amount, meter, resourceId, subscriptionId, GetNow() };
             var expectedTemplate = AuditClientConsumptionMetricsDestination.Template;
             auditMock.Verify(a => a.Write(expectedTemplate, expectedPropertyValues), times: Times.Once);
             if (reason != null)
             {
-                auditMock.Verify(a => a.ForContext("Reason", reason, false), Times.Once);
+                auditMock.Verify(a => a.ForContext("z", reason, false), Times.Once);
             }
 
             foreach (var (name, value) in internalContext)
@@ -146,7 +150,7 @@ namespace Kmd.Logic.Consumption.Client.Tests
         public void TheDefaultTemplateIsCorrect()
         {
             AuditClientConsumptionMetricsDestination.Template.Should()
-                .Be("Consumed {Amount} for {Meter} on resource {ResourceId} in subscription {SubscriptionId} recorded DateTime {ConsumedDatetime}");
+                .Be("Consumed {Amount} for {Meter} on resource {ResourceId} at {ConsumedDatetime} in subscription {SubscriptionId}");
         }
 
         [Theory]
